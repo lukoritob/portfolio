0bc572ee05b8741f36c9b99cd720917953564ef8
Brian Lascelles <lukoritob@gmail.com>
2023-06-24 17:52:29 +0300

    Deploy application

--- src/Assets/projectsData.js
+++ src/Assets/projectsData.js
@@ -4,34 +4,34 @@ export const projectsData = [
         projectDescription:
             'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
         imageUrl: 'https://raw.githubusercontent.com/lukoritob/portfolio-images/main/proj1.PNG',
-        projectUrl: 'http://theleanprogrammer.com/name-it/',
+        projectUrl: 'https://github.com/lukoritob/nameit',
     },
     {
         projectName: 'FlashType',
         projectDescription:
             'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
         imageUrl: 'https://raw.githubusercontent.com/lukoritob/portfolio-images/main/proj2.PNG',
-        projectUrl: 'http://theleanprogrammer.com/flashtype/',
+        projectUrl: 'https://github.com/lukoritob/typing-test-App',
     },
     {
         projectName: 'Flexbox Cheatsheet',
         projectDescription:
             'A frontend web developer must know the concepts of CSS Flexbox. So here is a CSS Flexbox cheatsheet, built using CSS Flexbox. Learn the concepts of flexbox by building a cheatsheet which is built using flexbox. Super cool!',
         imageUrl: 'https://raw.githubusercontent.com/lukoritob/portfolio-images/main/proj3.PNG',
-        projectUrl: 'http://theleanprogrammer.com/flex/',
+        projectUrl: '#',
     },
     {
         projectName: 'PokeDex',
         projectDescription:
             "Pokemon was a huge part of our childhood, wasn't it? Talking about pokemon brings back so many memories, but do you remember the details of each pokemon? Probably No! So here I bring you a pokedex to check all pokemons :)",
         imageUrl: 'https://raw.githubusercontent.com/lukoritob/portfolio-images/main/proj4.PNG',
-        projectUrl: 'http://theleanprogrammer.com/pokedex/',
+        projectUrl: 'https://github.com/lukoritob/Pokemon-Search-App',
     },
     {
         projectName: 'Portfolio Website',
         projectDescription:
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one.',
         imageUrl: 'https://raw.githubusercontent.com/lukoritob/portfolio-images/main/proj5.PNG',
-        projectUrl: 'http://lukoritob.tech/portfolio/',
+        projectUrl: 'https://lukoritob.github.io/portfolio/#/',
     },
 ];
